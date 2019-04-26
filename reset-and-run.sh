2b5ae19675d9d511b53e2342d490c80bf60252ad
Scott Langridge <scott.m.langridge@gmail.com>
2019-04-26 13:27:45 +0000

    Crudely detects motion

---
+++ reset-and-run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf images
+mkdir images
+python run.py
--- run.py
+++ run.py
@@ -2,36 +2,43 @@ import os
 from PIL import Image
 from time import sleep
 from math import sqrt
+import statistics as stat
 
 
 FILENAME = 'images/cap.jpg'
-LOOP_DELAY = 1
+LOOP_DELAY = 2
 
 
 def main_loop():
-    img_bank = setup()
-    oldest = 0
+    calibration = calibrate()
+    mean = calibration[1]
+    deviation = calibration[2]
+    recently_changed = calibration[3]
 
+    print('Running...')
     while True:
         pixels = capture()
 
-        if detect_motion(pixels, img_bank):
+        detect_result = detect_motion(pixels, mean, deviation,
+                                      recently_changed)
+        recently_changed = detect_result[1]
+        if detect_result[0]:
             detected()
 
-        img_bank[oldest] = pixels
-        oldest = (oldest + 1) % 10
         sleep(LOOP_DELAY)
 
 
-def setup():
+def calibrate():
     # Take a bank of reference images
+    print('Calibrating')
     img_bank = []
     for i in range(10):
-        print('Capturing reference images (%i/%i)' % (i + 1, 10))
+        print('Capturing calibration images (%i/%i)' % (i + 1, 10))
         img_bank.append(capture())
-        # sleep(LOOP_DELAY)
+        sleep(LOOP_DELAY)
 
-    # Get the mean colour values for each pix
+    # Get stats for stability of pixels
+    print('Processing')
     sum_pix = []
     for i in range(len(img_bank[0])):
         sum_pix.append([0, 0, 0])
@@ -57,10 +64,16 @@ def setup():
             std_devience = sqrt(sum_devience / len(img_bank))
             val.append(std_devience)
         dev_pix.append(val)
-    print(dev_pix)
-    input()
 
-    return img_bank
+    # Get stats for stability of images
+    recently_changed = detect_motion(capture(), mean_pix, dev_pix, [0,
+                                                                    2500])[1]
+    for i in range(4):
+        recently_changed = detect_motion(capture(), mean_pix, dev_pix,
+                                         recently_changed)[1]
+    print('Calibration complete')
+
+    return img_bank, mean_pix, dev_pix, recently_changed
 
 
 def capture():
@@ -69,14 +82,33 @@ def capture():
     return list(im.getdata())
 
 
-def detect_motion(pixels, img_bank):
-    pass
-    # TODO Detect if something has moved or not here
+def detect_motion(image, mean, pix_devs, changed_recently):
+    # Count changed pixels
+    changed_pixels = 0
+    for pix in range(len(image)):
+        for i in range(3):
+            if abs(image[pix][i] - mean[pix][i]) > pix_devs[pix][i] * 2:
+                changed_pixels += 1
+
+    # Determine if significant
+    mean = sum(changed_recently) / len(changed_recently)
+    deviation = abs(changed_pixels - mean)
+    if deviation > stat.stdev(changed_recently) * 2:
+        movement = True
+    else:
+        movement = False
+
+    # Update changed_recently
+    if len(changed_recently) >= 10:
+        changed_recently = changed_recently[1:]
+    changed_recently.append(changed_pixels)
+
+    # Return result
+    return movement, changed_recently
 
 
 def detected():
-    pass
-    # TODO Code for when detected goes here
+    print('MOTION DETECTED')
 
 
 main_loop()
